195f8af477b7f61a9c890aab448fdc71b82a5259
larissacybyk <jdj9ng@virginia.edu>
2024-12-02 15:43:13 -0500

    initial commit

---
+++ src/main/java/com/example/cs4750/Cs4750Application.java
@@ -0,0 +1,13 @@
+package com.example.cs4750;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs4750Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs4750Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cs4750/Cs4750ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs4750;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs4750ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
