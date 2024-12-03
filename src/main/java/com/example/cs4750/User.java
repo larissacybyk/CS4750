5fbec8c4bc7f6f4832d955e8ca758a536ce3f81d
Kayla Nguyen <kaylalngn@gmail.com>
2024-12-03 10:17:12 -0500

    user entity

---
+++ src/main/java/com/example/cs4750/User.java
@@ -0,0 +1,49 @@
+package com.example.cs4750;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class Users {
+    @Id
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    public Users() {}
+
+    public Users(int userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
